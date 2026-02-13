9d695f8e32aa4cb610555be8e6b86ea6e5d052ac
Tom Weber <w.tom0405@gmail.com>
2026-02-13 14:15:46 +0100

    [OK] chnage the color of active navbar link active

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function loadComponent(id, path) {
-    fetch(path)
+    return fetch(path)
     .then(response => response.text())
     .then(data => {
         document.getElementById(id).innerHTML = data;
@@ -8,20 +8,32 @@ function loadComponent(id, path) {
 }
 
 function changeActivePropertyLinkHeader() {
-    console.log('Changing active property link in header');
+    // get the current path
     const path = window.location.pathname;
-    console.log(path);
-    const propertyLink = document.querySelector('.property-link');
-    if (path.includes('property.html')) {
-        propertyLink.classList.add('active');
-    } else {        propertyLink.classList.remove('active');
+    const page = path.split('/').pop();
+
+    // get all the links in the header with class navbar-link
+    const links = document.getElementsByClassName('navbar-link');
+
+    // loop through the links and add the active class to the one that matches the current path
+    for (let i = 0; i < links.length; i++) {
+        const link = links[i];
+
+        // get the href attribute of the link and compare it to the current page 
+        if (link.getAttribute('href') === page) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
     }
 }
 
 function main() {
-    loadComponent('header-placeholder', './components/header.html');
+    // load the header and footer components
+    // after loading the header, call the function to change the active link
+    loadComponent('header-placeholder', './components/header.html')
+    .then(() => changeActivePropertyLinkHeader());
     loadComponent('footer-placeholder', './components/footer.html');
-    // changeActivePropertyLinkHeader();
 }
 
 main();
