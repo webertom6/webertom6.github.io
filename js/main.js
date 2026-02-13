e00c196831038b164b6ad374a6fb269c0cdf202c
Tom Weber <w.tom0405@gmail.com>
2026-02-13 14:00:26 +0100

    [OK] add modular header/footer template, css project card, SEO title

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-function loadHeader() {
-    fetch('./components/header.html')
+function loadComponent(id, path) {
+    fetch(path)
     .then(response => response.text())
-    .then(data => document.querySelector('header').innerHTML = data);
+    .then(data => {
+        document.getElementById(id).innerHTML = data;
+    })
+    .catch(err => console.error('Error loading component:', err));
 }
 
-function loadFooter() {
-    fetch('./components/footer.html')
-    .then(response => response.text())
-    .then(data => document.querySelector('footer').innerHTML = data);
+function changeActivePropertyLinkHeader() {
+    console.log('Changing active property link in header');
+    const path = window.location.pathname;
+    console.log(path);
+    const propertyLink = document.querySelector('.property-link');
+    if (path.includes('property.html')) {
+        propertyLink.classList.add('active');
+    } else {        propertyLink.classList.remove('active');
+    }
 }
+
+function main() {
+    loadComponent('header-placeholder', './components/header.html');
+    loadComponent('footer-placeholder', './components/footer.html');
+    // changeActivePropertyLinkHeader();
+}
+
+main();
